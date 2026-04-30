857f9438140a9ff4dab63f3585363e4f9a56f638
samkiaVTK <samkiasamkiasamkia@gmail.com>
2026-04-30 15:15:41 +0200

    uploaded code changes

--- index.js
+++ index.js
@@ -19,10 +19,21 @@ function loadWebView() {
   win.contentView.addChildView(view)
   view.webContents.loadURL('https://google.com')
   view.setBounds({ x: 0, y: 50, width: 800, height: 550 })
+    view.webContents.on('did-navigate', (event, url) => {//callback when changing URL
+    win.webContents.send('url-changed', url)
+  })
 }
 
 ipcMain.on('go-back', () => {
   if (view.webContents.canGoBack()) view.webContents.goBack()
+})
+
+ipcMain.on('go-forward', () => {
+  if (view.webContents.canGoForward()) view.webContents.goForward()
+})
+
+ipcMain.on('reload', () => {
+  view.webContents.reload()
 })
 
 ipcMain.on('load-url', (event, url) => {
--- preload.js
+++ preload.js
@@ -2,5 +2,13 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('api', {
   goBack: () => ipcRenderer.send('go-back'),
+  goForward: () => ipcRenderer.send('go-forward'),
+  reload: () => ipcRenderer.send('reload'),
   loadURL: (url) => ipcRenderer.send('load-url', url),
+
+  onUrlChange: (callback) => {
+    ipcRenderer.on('url-changed', (event, url) => {
+      callback(url)
+    })
+  }
 })
