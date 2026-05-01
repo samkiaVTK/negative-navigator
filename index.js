6869cace7b94a62fdedacb5d36622064c39a8737
samkiaVTK <samkiasamkiasamkia@gmail.com>
2026-05-01 15:14:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, WebContentsView, ipcMain } = require('electron')
+const { app, BrowserWindow, WebContentsView, ipcMain, Menu } = require('electron')
 let win
 let view
 
@@ -19,17 +19,17 @@ function loadWebView() {
   win.contentView.addChildView(view)
   view.webContents.loadURL('https://google.com')
   view.setBounds({ x: 0, y: 50, width: 800, height: 550 })
-    view.webContents.on('did-navigate', (event, url) => {//callback when changing URL
+  view.webContents.on('did-navigate', (event, url) => {//callback when changing URL
     win.webContents.send('url-changed', url)
   })
 }
 
 ipcMain.on('go-back', () => {
-  if (view.webContents.canGoBack()) view.webContents.goBack()
+  if (view.webContents.navigationHistory.canGoBack()) view.webContents.navigationHistory.goBack()
 })
 
 ipcMain.on('go-forward', () => {
-  if (view.webContents.canGoForward()) view.webContents.goForward()
+  if (view.webContents.navigationHistory.canGoForward()) view.webContents.navigationHistory.goForward()
 })
 
 ipcMain.on('reload', () => {
